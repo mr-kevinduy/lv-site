d54b81cc77c163991c49a6f29e36cd02d59fbd19
Kevin Duy <kevinduy@Kevins-MacBook-Pro.local>
2024-07-14 23:37:23 +0700

    Auth api with breeze

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
 
-Route::get('/demo', function () {
-    return view('demo');
-});
-
-require_once dirname(__FILE__).'/admin.php';
+require __DIR__.'/auth.php';
